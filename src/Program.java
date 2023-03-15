0d7d8d97b2083935c3a9c9684f113ddf345fa780
Santiago Gallego Henao <santiago.gallegoh@upb.edu.co>
2023-03-15 14:17:23 -0600

    init

---
+++ src/Program.java
@@ -0,0 +1,30 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("================ D A G R A N =======================");
+
+        //Aqui pedimos cuantos zonas se van a inicializar
+        Scanner sc = new Scanner(System.in);
+        int cantidadZonas = 0;
+        boolean datoCorrecto = false;
+
+        do {
+            try {
+                System.out.print("\nCuantas Zonas desea inicializar: ");
+                cantidadZonas = sc.nextInt();
+                if (cantidadZonas > 0)
+                    datoCorrecto = true;
+                else {
+                    System.out.println("Error; Ingresa una cantidad valida de Zonas");
+                }
+            } catch (InputMismatchException errorDato) {
+                System.out.println("Error; Ingrese correctamente el dato, por favor digital el numero de zonas nuevamente");
+                sc.next();
+            }
+        }
+        while (!datoCorrecto);
+    }
+
+}
---
+++ src/Zona.java
@@ -0,0 +1,15 @@
+public abstract class Zona {
+
+    protected int nivelDelMar;
+    protected int area;
+    protected int totalHabitantes;
+    protected int distanciaRios;
+    
+
+
+
+
+
+
+    protected abstract void TipoDeZona();
+}
